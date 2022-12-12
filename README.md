ed03c0d3adad8860edb4c40d4924070cac0fbf71
tronborg2000 <colinweldon1980@gmail.com>
2022-12-12 18:03:25 +0000

    fix constant uppercase errors

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
-Battleship
+## Battleship
 Battleship is a simple Python script that allows you to play a game of battleship against the computer.
 
-Requirements
+## Requirements
 To run this script, you will need to have Python installed on your computer.
 
-How to play
+## How to play
 To play the game, simply run the battleship.py script. The computer will place two battleships on a 5x5 grid, and you can enter coordinates to try to hit the battleships. If you hit a battleship, the space will be marked with an 'X' and the ship will be removed from the grid. If you miss, the space will be marked with an 'O'. The game ends when all battleships have been destroyed.
 
-Customizing the game
+## Customizing the game
 You can customize the game by changing the values of the GRID_SIZE and NUM_SHIPS variables at the top of the battleship.py script. You can also modify the rules of the game by changing the code in the main game loop.
 
-Contributions
+## Contributions
 If you would like to contribute to this project, you can submit a pull request on GitHub.
 
-License
-This project is licensed under the MIT License. See the LICENSE file for more details.
+## Bugs: 
+
+I was getting an error message ValueError: invalid literal for int() with base 10: 'x2' As I was entering coordinates with an X or Y value before the numbers entered. 
+
--- run.py
+++ run.py
@@ -7,48 +7,48 @@ GRID_SIZE = 5
 # Create the grid
 grid = []
 for i in range(GRID_SIZE):
-  row = []
-  for j in range(GRID_SIZE):
-    row.append('-')
-  grid.append(row)
+    row = []
+    for j in range(GRID_SIZE):
+        row.append('-')
+    grid.append(row)
 
 # Place the battleships on the grid
 # (in this example, we will place two battleships on the grid)
 ships = []
 for i in range(2):
-  placed = False
-  while not placed:
-    x = random.randint(0, GRID_SIZE - 1)
-    y = random.randint(0, GRID_SIZE - 1)
-    if grid[x][y] == '-':
-      grid[x][y] = 'B'
-      ships.append((x, y))
-      placed = True
+    PLACED = False
+    while not PLACED:
+        x = random.randint(0, GRID_SIZE - 1)
+        y = random.randint(0, GRID_SIZE - 1)
+        if grid[x][y] == '-':
+            grid[x][y] = 'B'
+            ships.append((x, y))
+            placed = True
 
 # Main game loop
 while True:
-  # Print the grid
-  for row in grid:
-    print(' '.join(row))
+    # Print the grid
+    for row in grid:
+        print(' '.join(row))
 
-  # Prompt the user for their move
-  x = int(input('Enter x coordinate: '))
-  y = int(input('Enter y coordinate: '))
+    # Prompt the user for their move
+    x = int(input('Enter x coordinate: '))
+    y = int(input('Enter y coordinate: '))
 
-  # Check if the user hit a battleship
-  hit = False
-  for ship in ships:
-    if x == ship[0] and y == ship[1]:
-      grid[x][y] = 'X'
-      ships.remove(ship)
-      hit = True
-      break
+    # Check if the user hit a battleship
+    HIT = False
+    for ship in ships:
+        if x == ship[0] and y == ship[1]:
+            grid[x][y] = 'X'
+            ships.remove(ship)
+            hit = True
+            break
 
-  # If the user didn't hit a battleship, mark the space as missed
-  if not hit:
-    grid[x][y] = 'O'
+    # If the user didn't hit a battleship, mark the space as missed
+    if not hit:
+        grid[x][y] = 'O'
 
-  # Check if the game is over
-  if len(ships) == 0:
-    print('You won!')
+    # Check if the game is over
+    if len(ships) == 0:
+        print('You won!')
     break
