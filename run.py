2f8943c818a6de97c07d4fffca04afbb197ee6eb
tronborg2000 <colinweldon1980@gmail.com>
2022-12-12 14:19:17 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import copy as c
+import os
+import random as rand
+
+"""
+Defining the Game class to make it easier to 
+understand the code further in the project
+"""
+
+
+class Game(object):
+    def __init__(self, players):
+        self.guesses = 5
+        self.player_list = []
+        for player in range(players):
+            self.player_list.append(self.guesses)
+        self.current_player = 1
+        self.board = self.create_matrix(5, 5)
+        self.board_visible = c.deepcopy(self.board)
+        self.ship_row = rand.randint(0, 4)
+        self.ship_col = rand.randint(0, 4)
+        self.guess_row = 0
+        self.guess_col = 0
